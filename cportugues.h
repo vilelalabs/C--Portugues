9a79c00000bfbb21b81e622f4a4f763e50dd07fd
hvilela0 <hvilela0@gmail.com>
2021-09-21 10:08:58 -0300

    Initial commit

---
+++ cportugues.h
@@ -0,0 +1,111 @@
+#ifndef __CPORTUGUES__H__
+#define __CPORTUGUES__H__
+
+#include <stdio.h>
+
+#define alinhacomo alignas
+#define alinhade alignof
+#define e and
+#define e_igual and_eq
+//asm
+//auto
+#define bite bitand
+#define bitou bitor
+#define booleano bool
+#define parar break
+#define caso case
+#define pega catch
+#define caractere char
+#define caractere_16_t char16_t
+#define caractere_32_t char32_t
+#define classe class
+#define complemento compl //?
+#define constante const
+#define expressaoconstante constexpr
+#define chamada_constante const_cast
+#define continua continue
+#define declaratipo decltype
+#define padrao default
+#define deleta delete
+#define fazer do
+#define flutuantemuitolongo double
+#define chamada_dinamica dynamic_cast
+#define senao else
+#define enumera enum
+#define explicito explicit
+#define exporta export
+#define externo extern
+#define falso false
+#define pontoflutuante float
+#define para for
+#define amigo friend
+#define vapara goto
+#define se if
+#define funcao_principal main
+#define nalinha inline
+#define inteiro int
+#define flutuantelongo long
+#define mutavel mutable
+#define espaco_de_nomes namespace
+#define novo new
+#define semexceção noexcept
+#define não not
+#define não_igual not_eq
+#define ponteironulo nullptr
+#define operador operator
+#define ou or
+#define ou_igual or_eq
+#define privado private
+#define protegido protected
+#define público public
+#define registro register
+#define chamada_reinterpretada reinterpret_cast
+#define retorno return
+#define pequeno short
+#define comsinal signed
+#define tamanhode sizeof
+#define estatico static
+#define declaração_estatica static_assert
+#define chamada_estatica static_cast
+#define estrutura struct
+#define trocar switch
+#define modelo template
+#define este this
+#define thread_local local_thread
+#define lancar throw
+#define verdadeiro true
+#define tentar try
+#define definetipo typedef
+#define defineidentificação typeid
+#define tipodenome typename
+#define união union
+#define semsinal unsigned
+#define usando using
+//virtual
+#define vazio void
+#define volatil volatile
+//wchar_t
+#define enquanto while
+#define exclusivoou xor
+#define exclusivoou_igual xor_eq
+
+#define diferente_de !=
+#define igual_a ==
+#define maior_que >
+#define maior_ou_igual_a >=
+#define menor_que <
+#define menor_ou_igual_a <=
+#define vira =
+
+#define vezes *
+#define DIVIDIDO_POR /
+#define módulo_de %
+#define mais +
+#define menos -
+#define mais_um ++
+#define menos_um --
+
+#define escreve printf
+#define lê scanf
+
+#endif //!__CPORTUGUES__H__
